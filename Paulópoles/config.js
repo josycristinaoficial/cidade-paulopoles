4cb98a8c3c3e0a24db560052ac07940a76e376e0
josycristinaoficial <josycristinaoficial@gmail.com>
2026-05-28 13:26:01 -0300

    Add files via upload

--- Paulópoles/config.js
+++ Paulópoles/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  googleScriptUrl: "",
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbyWH8ogP5aM8G4bZPVPqFnXVY84_kMKG-fAgoDpv3aTCXjP9bwnHfK7vHgxZnaXK6mYUA/exec",
   preRegistrationUrl: "https://docs.google.com/forms/d/e/1FAIpQLSePWqeOb0f3cPCe9O4XhWcWsaEoOUq2MQ1OL6cLugEjUTqXYQ/viewform",
   maxFileSizeMb: 10
 };
